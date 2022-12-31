1453b09441ced7b89deac959e5a2cd07e3d7c735
ealande <erickalan23@gmail.com>
2022-12-31 11:48:11 -0300

    Template string

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,44 @@
-alert ("Hello, World")
-console.log("Hello, world 2.0")
-
-let names ="test"
-let age = 90.5
-let state = "Parana"
-let city = "Curitiba"
-let logged = true //or false, used to show if a user is logged or not. Its a boolean variable.
-let lists = ["egg","pasta", "tomatoes"] //array and objects are classified as an object variable
-let carBrand = "Ferrari"
-let price = 19.99
-let foreingcity = "Tokyo"
-let fullName = {names: "Erick Lima", age: 30}
+alert ("Hello, World");
+console.log("Hello, world 2.0");
+//let var const
+let names ="test";
+let age = 90.5;
+let state = "Parana";
+let city = "Curitiba";
+let logged = true; //or false, used to show if a user is logged or not. Its a boolean variable.
+let lists = ["egg","pasta", "tomatoes"]; //array and objects are classified as an object variable
+let carBrand = "Ferrari";
+let price = 19.99;
+let foreingcity = "Tokyo";
+let fullName = {names: "Erick Lima", age: 30};
+let x = 2;
+let y = "example";
+let carModel = "LaFerrari";
+let whatCar = carBrand + ' ' + carModel;
+x = x/8;
+y = y+10;
+let newCarBrand = "Ford";
+let newCarModel = "Mustang";
+let newCar = `${newCarBrand} ${newCarModel}`;//template string
+let fullage = `${(age + 4) + "bla"} years old`;
 /*console.log is used to 
-insert information in the console*/
-console.log (names)
-console.log (age)
-console.log (city)
-console.log (state)
-console.log (carBrand)
-console.log (price)
-console.log (foreingcity)
-console.log (typeof names)
-console.log (typeof age)
-console.log (typeof logged)
-console.log (typeof lists)
-console.log (typeof fullName) 
-
+show some information in the console*/
+console.log (names);
+console.log (age);
+console.log (city);
+console.log (state);
+console.log (fullName);
+console.log (carBrand); console.log (price); console.log (foreingcity);
+console.log (typeof names);
+console.log (typeof age);
+console.log (typeof logged);
+console.log (typeof lists);
+console.log (typeof fullName); 
+names = "Erick Lima";
+console.log (names);
+console.log (whatCar)
+console.log (x);
+console.log (y);
+console.log ("Brand and model of the car:" + whatCar);
+console.log ("Brand and model of the car:" + newCar);
+console.log (fullage);
