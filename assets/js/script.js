449a721e29cf7770f6529dbd5b399018b28ec35a
ealande <erickalan23@gmail.com>
2022-12-31 09:38:30 -0300

    trying new things at JS language and learning GIT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 alert ("Hello, World")
 console.log("Hello, world 2.0")
+
+let names ="test"
+let age = 90.5
+let state = "Parana"
+let city = "Curitiba"
+let logged = true //or false, used to show if a user is logged or not. Its a boolean variable.
+let lists = ["egg","pasta", "tomatoes"] //array
+let fullName = {names: "Erick Lima", age: 30}
+
+console.log (names)
+console.log (age)
+console.log (city)
+console.log (state)
+console.log (typeof names)
